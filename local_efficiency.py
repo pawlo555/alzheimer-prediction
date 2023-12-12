d8380add27fa447a55b584df69e248f906088432
Mikolaj <m_zasada@me.com>
2023-12-12 11:42:29 +0100

    Laboratories update

--- local_efficiency.py
+++ local_efficiency.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from utils import load_data
 
@@ -10,10 +11,10 @@ if __name__ == '__main__':
 
     local_efficiencies = []
     global_efficiencies = []
-    for i in range(X.shape[0]):
+    for i in tqdm(range(X.shape[0])):
         # Create a graph from the adjacency matrix
         G = nx.from_numpy_array(X[i])
-        print(str(i))
+        # print(str(i))
         local_efficiencies.append(nx.local_efficiency(G))
         global_efficiencies.append(nx.global_efficiency(G))
 
@@ -44,3 +45,11 @@ if __name__ == '__main__':
     plt.hist(hists, label=labels)
     plt.legend()
     plt.show()
+
+
+# Local efficiency in per nodes wit networkx
+# Apply this results in predicting alzheimer
+# Compare groups for each pair based on global efficiency
+# AD vs EMCI and EMCI vs NC ect.
+# Based on local efficiency we can perform feature importance
+# boxplots for global efficiency
